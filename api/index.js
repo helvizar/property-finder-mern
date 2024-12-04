e0a44d1f20499949fceaa3aedae9e02ef16d9b84
Helvizar <helvizaranderson@gmail.com>
2024-12-04 23:58:07 +0700

    feat: install and connect mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect(
+	"mongodb+srv://helvizar:helvizar@property-finder-mern.fura2.mongodb.net/?retryWrites=true&w=majority&appName=property-finder-mern"
+);
 
 const app = express();
 
