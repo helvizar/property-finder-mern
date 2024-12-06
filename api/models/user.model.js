042b42f10bcb3a573f4f3ac8744567d1f4363cb4
Helvizar <helvizaranderson@gmail.com>
2024-12-06 20:40:29 +0700

    chore: minor update 2 user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,27 +1,24 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    require: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    require: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    require: true,
-    validate: {
-      // Regex untuk validasi password
-      const passwordRegex = /^(?=.*[A-Z])(?=.\d)(?=.*[!@#$%^&*(),.?":{}|<>])$/
-      return passwordRegex.test(value);
-    },
-    message: "Password harus mengandung angka, huruf kapital, dan simbol."
-  }
-}, { timestamps: true });
+const userSchema = mongoose.Schema(
+	{
+		username: {
+			type: String,
+			require: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			require: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			require: true,
+		},
+	},
+	{ timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
